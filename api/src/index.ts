586d32c6d13dd47846a90918a4d8f21097ad345d
Claudius A. <ayadiclaudius@gmail.com>
2025-03-24 17:28:36 +0100

    chore: enhance API middleware with CORS and logging support

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,12 @@
 import { cors } from 'hono/cors';
 import { env } from '@/utils/types';
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 import recommender from './routes/recommend';
 
 const app = new Hono();
-app.use('*', cors());
+app.use('*', cors({ origin: '*' }));
+app.use('*', logger());
 
 const routes = app.basePath('/api/v1').route('/recommend', recommender);
 
