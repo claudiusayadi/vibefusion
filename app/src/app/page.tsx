2ea6d2cb05034b454e0b8c74267055d15032b6fe
Claudius A. <ayadiclaudius@gmail.com>
2025-03-28 16:32:58 +0100

    feat(app): simplify Home component to display a greeting message instead of movie list

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,27 +1,7 @@
 import fetcher from '@/utils/fetcher';
-import { Movie } from '@/utils/types';
-import Image from 'next/image';
 
 export default async function Home() {
 	const data = await fetcher('recommend', 'tired and worn out');
 	console.log(data);
-	return (
-		<ul>
-			{data.movies.map((movie: Movie) => (
-				<li key={movie.id}>
-					<Image
-						src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-						alt={movie.title}
-						width={500}
-						height={750}
-					/>
-					<h2>{movie.title}</h2>
-					<p>{movie.overview}</p>
-					<p>
-						Rating: {movie.vote_average} | Released: {movie.release_date}
-					</p>
-				</li>
-			))}
-		</ul>
-	);
+	return <p className='text-center font-bold'>Hello from Next.js</p>;
 }
