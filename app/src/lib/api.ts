eab8460b3ee16aa6f93269c890fb668ce0252131
Claudius A. <ayadiclaudius@gmail.com>
2025-03-28 14:58:59 +0100

    feat(app): refactor Home component to fetch and display movie recommendations

--- app/src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import fetcher from '@/utils/fetcher';
-import { ApiResponse } from '@/utils/types';
-
-export const getRecommendations = async (
-	query: string
-): Promise<ApiResponse> => {
-	return await fetcher(query);
-};
